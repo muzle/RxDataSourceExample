f97e503881ef28ec1a3c2b005f04281dc6f09300
Eugene Rudakov <voragomod@icloud.com>
2022-11-27 23:09:22 +0600

    initial commit

---
+++ DataSourceExample/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  DataSourceExample
+//
+//  Created by muzle on 27.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
